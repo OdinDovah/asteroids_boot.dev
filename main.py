909e0dac7e4a2ba16a205a2f0a35e79f76a3def9
OdinDovah <pro@albertson-clan.com>
2025-08-06 17:33:01 -0400

    Grouped updatable and drawable items

--- main.py
+++ main.py
@@ -9,14 +9,18 @@ def main():
     clk = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        player.update(dt)
         screen.fill(color="#000000")
-        player.draw(screen)
+        updatable.update(dt)
+        for item in drawable:
+            item.draw(screen)
         pygame.display.flip()
         dt = clk.tick(60) / 1000
     print("Starting Asteroids!")
