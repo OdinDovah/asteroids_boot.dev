0cded12dc68ca4794c830ac829749f469161bae4
OdinDovah <pro@albertson-clan.com>
2025-08-06 16:27:14 -0400

    Added player sprite that can spin left and right

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
 import pygame
+from pygame.time import Clock
 from constants import *
+from player import *
 
 
 def main():
     pygame.init()
+    clk = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        player.update(dt)
         screen.fill(color="#000000")
+        player.draw(screen)
         pygame.display.flip()
+        dt = clk.tick(60) / 1000
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
